a3d9505fe368f6987f05104461eeb2aa10c61c0d
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2026-09-15 01:40:37 +0900

    initial commit

---
+++ src/main/java/_jbbm/keepgo/KeepgoApplication.java
@@ -0,0 +1,13 @@
+package _jbbm.keepgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_jbbm/keepgo/KeepgoApplicationTests.java
@@ -0,0 +1,13 @@
+package _jbbm.keepgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
