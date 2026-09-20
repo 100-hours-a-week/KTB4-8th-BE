f9994baba760e4245a99d382442990a1c1d94fce
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2026-09-21 02:19:38 +0900

    feat: 회원 정보 수정 기능 구현
    회원 정보 수정 기능 구현을 위해 DTO를 구현하였음

--- src/main/java/eightjbbm/keepgo/member/dto/UpdateMemberInfoResponse.java
+++ src/main/java/eightjbbm/keepgo/member/dto/UpdateMemberInfoResponse.java
@@ -1,7 +1,10 @@
 package eightjbbm.keepgo.member.dto;
 
+/// @param nickname 회원의 닉네임
+/// @param profileImageUrl 회원의 프로필 사진 주소
 public record UpdateMemberInfoResponse(
-
+        String nickname,
+        String profileImageUrl
 ) {
     public static UpdateMemberInfoResponse from(UpdateMemberInfoResult result) {
         return null;
