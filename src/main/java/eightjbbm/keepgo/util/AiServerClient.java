c1da66f3718332d44ab7d35c1fa0ec343705c1a0
LifeSatin <dive0dice@gmail.com>
2026-09-20 10:00:35 +0900

    add ai server client

---
+++ src/main/java/eightjbbm/keepgo/util/AiServerClient.java
@@ -0,0 +1,18 @@
+package eightjbbm.keepgo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AiServerClient {
+    public void recommendCourse() {
+
+    }
+
+    public void extractSlot() {
+
+    }
+
+    public void analyzeVideo() {
+
+    }
+}
