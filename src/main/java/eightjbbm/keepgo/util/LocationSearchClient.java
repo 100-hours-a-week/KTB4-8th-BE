d83348365ef3ff363794aaaf2d56774a4cfe4a3c
LifeSatin <dive0dice@gmail.com>
2026-09-20 10:01:04 +0900

    add location search api(행정안전부) client

---
+++ src/main/java/eightjbbm/keepgo/util/LocationSearchClient.java
@@ -0,0 +1,15 @@
+package eightjbbm.keepgo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocationSearchClient {
+
+    public void searchByKeyword() {
+
+    }
+
+    private LocationSearchResponse searchByKeywordApi(String keyword) {
+
+    }
+}
---
+++ src/main/java/eightjbbm/keepgo/util/LocationSearchResponse.java
@@ -0,0 +1,21 @@
+package eightjbbm.keepgo.util;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public record LocationSearchResponse(
+        Common common,
+        Juso juso
+) {
+    public record Common(
+            String totalCount,
+            Integer currentPage,
+            Integer countPerPage
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Juso(
+            String siNm,
+            String sggNm,
+            String emdNm
+    ) {}
+}
