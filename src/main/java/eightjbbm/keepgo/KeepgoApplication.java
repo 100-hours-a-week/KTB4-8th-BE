6b98c2ce966cb14030fe5f6b3039632c3f58403b
LifeSatin <dive0dice@gmail.com>
2026-09-23 13:26:48 +0900

    refactor: 불필요한 import 제거

--- src/main/java/eightjbbm/keepgo/KeepgoApplication.java
+++ src/main/java/eightjbbm/keepgo/KeepgoApplication.java
@@ -2,10 +2,6 @@ package eightjbbm.keepgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.server.authorization.token.DefaultOAuth2TokenContext;
-import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 
 @SpringBootApplication
 public class KeepgoApplication {
