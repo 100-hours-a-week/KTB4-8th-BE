10e3d12f534b137a388ff4498c1f24762015d79e
LifeSatin <dive0dice@gmail.com>
2026-09-23 13:24:43 +0900

    feat: 여정 코스 추천 요청 기능의 서비스 계층 응답 DTO 구현

--- src/main/java/eightjbbm/keepgo/recommendation/dto/RequestRecommendationResult.java
+++ src/main/java/eightjbbm/keepgo/recommendation/dto/RequestRecommendationResult.java
@@ -1,4 +1,87 @@
 package eightjbbm.keepgo.recommendation.dto;
 
-public record RequestRecommendationResult() {
+import eightjbbm.keepgo.util.dto.RecommendCourseResponse;
+
+import java.util.List;
+
+public record RequestRecommendationResult(
+        RecommendationMetadata metadata,
+        List<RecommendationSnippet> snippets,
+        List<RecommendationDetail> details
+
+) {
+    public record RecommendationMetadata(
+            Integer totalCourseCount,
+            String location,
+            String scheduledTime
+    ) {}
+
+    public record RecommendationSnippet(
+            String title,
+            String recommendationScore,
+            Integer totalPlaceCount,
+            Integer totalTravelTime
+    ) {
+        public static RecommendationSnippet from(RecommendCourseResponse.RecommendData.RecommendCourse course) {
+            return new RecommendationSnippet(
+                    course.title(),
+                    String.valueOf(100),
+                    course.places().size(),
+                    course.places()
+                            .stream()
+                            .mapToInt(RecommendCourseResponse.RecommendData.RecommendCourse.RecommendPlace::travelMinutes)
+                            .sum()
+            );
+        }
+    }
+
+    public record RecommendationDetail(
+            String title,
+            String recommendationScore,
+            Integer totalTravelTime,
+            List<RecommendationDetailItem> items
+    ) {
+        public static RecommendationDetail from(RecommendCourseResponse.RecommendData.RecommendCourse course) {
+            return new RecommendationDetail(
+                    course.title(),
+                    String.valueOf(100),
+                    course.places().stream().mapToInt(RecommendCourseResponse.RecommendData.RecommendCourse.RecommendPlace::travelMinutes).sum(),
+                    course.places().stream().map(RecommendationDetailItem::from).toList()
+            );
+        }
+    }
+
+    public record RecommendationDetailItem(
+            Integer sequence,
+            String name,
+            String category,
+            String description,
+            Float lat,
+            Float lng
+    ) {
+        public static RecommendationDetailItem from(RecommendCourseResponse.RecommendData.RecommendCourse.RecommendPlace place) {
+            return new RecommendationDetailItem(
+                    null,
+                    place.placeName(),
+                    null,
+                    place.travelMinutes().toString(),
+                    null,
+                    null
+            );
+        }
+    }
+
+    public static RequestRecommendationResult from(
+            RequestRecommendationCommand command,
+            RecommendCourseResponse response) {
+        return new RequestRecommendationResult(
+                new RecommendationMetadata(
+                        response.data().courses().size(),
+                        command.location(),
+                        command.requestedTime().toString()
+                ),
+                response.data().courses().stream().map(RecommendationSnippet::from).toList(),
+                response.data().courses().stream().map().toList()
+        );
+    }
 }
