bbb186748ea4d95eeba5cc92f11022d5d016bbc6
LifeSatin <dive0dice@gmail.com>
2026-09-23 13:14:59 +0900

    feat: 비동기 처리 설정
    챗봇의 답변 생성을 폴링하기 위해 비동기 처리 관련 설정 진행

---
+++ src/main/java/eightjbbm/keepgo/util/AsyncConfig.java
@@ -0,0 +1,25 @@
+package eightjbbm.keepgo.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean(name = "httpTaskExecutor")
+    public Executor httpTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("http-task-");
+
+        executor.initialize();
+        return executor;
+    }
+}
